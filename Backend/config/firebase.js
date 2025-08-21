51f6d2f12ae3d0b5abc639bf998228e0a657d7b2
ARJUN-T-S <arjun.ts2022@vitstudent.ac.in>
2025-08-21 19:28:21 +0530

    Update firebase.js

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -2,7 +2,6 @@ const admin = require("firebase-admin");
 
 const serviceAccount = require("./firebase_login.json");
 
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
